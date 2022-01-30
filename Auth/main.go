486ddf04876b6f9f3c4680a7c9d7dfe4a1823d51
Rahul <rahulbhatia@Rahuls-MacBook-Pro.local>
2022-01-30 17:22:30 -0500

    Added Routers for AuthAPIs

--- Auth/main.go
+++ Auth/main.go
@@ -1,6 +1,35 @@
 package main
-import ("fmt")
-func main(){
 
-	fmt.println("Auth testing.")
+import (
+	"fmt"
+	"os"
+
+	routes "github.com/SimranBhagwandasani/EventPlanner/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("/api-1", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access Granted:1"})
+	})
+
+	router.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access Granted:2"})
+	})
+
+	router.RUN(":" + port)
+
+	fmt.Println("Auth testing.")
 }
