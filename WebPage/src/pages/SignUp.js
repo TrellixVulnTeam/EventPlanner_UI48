76e724cb973ed8af5940cf6fdf7c9bd41c533460
Jenil jain <54360821+jenil13@users.noreply.github.com>
2022-02-04 16:34:01 -0500

    Update SignUp.js

--- WebPage/src/pages/SignUp.js
+++ WebPage/src/pages/SignUp.js
@@ -27,15 +27,10 @@ const SignUp = () => {
                     <Typography variant='caption' gutterBottom>Please fill this form to create an account !</Typography>
                 </Grid>
                 <form>
-                    <TextField fullWidth label='Name' placeholder="Enter your name" />
+                    <TextField fullWidth label='First Name' placeholder="Enter your first name" />
+                    <TextField fullWidth label='Last Name' placeholder="Enter your last name " />
                     <TextField fullWidth label='Email' placeholder="Enter your email" />
-                    <FormControl component="fieldset" style={marginTop}>
-                        <FormLabel component="legend">Gender</FormLabel>
-                        <RadioGroup aria-label="gender" name="gender" style={{ display: 'initial' }}>
-                            <FormControlLabel value="female" control={<Radio />} label="Female" />
-                            <FormControlLabel value="male" control={<Radio />} label="Male" />
-                        </RadioGroup>
-                    </FormControl>
+                    
                     <TextField fullWidth label='Phone Number' placeholder="Enter your phone number" />
                     <TextField fullWidth label='Password' placeholder="Enter your password"/>
                     <TextField fullWidth label='Confirm Password' placeholder="Confirm your password"/>
