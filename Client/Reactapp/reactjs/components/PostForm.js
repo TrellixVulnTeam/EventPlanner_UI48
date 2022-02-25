66bafe301592b1eb21741357a02aef4fb9b88d5d
Jenil jain <54360821+jenil13@users.noreply.github.com>
2022-02-25 14:31:37 -0500

    Update PostForm.js

--- Client/Reactapp/reactjs/components/PostForm.js
+++ Client/Reactapp/reactjs/components/PostForm.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-class PostForm extends Component {
+import { Grid, Paper, Avatar, Typography, TextField, Button } from '@mui/material'
+// import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
+import { Radio } from '@mui/material';
+import { RadioGroup } from '@mui/material';
+// import FormControlLabel from '@mui/material';
+//  import {FormControl} from '@mui/material';
+import { FormControl } from '@mui/material';
+ import { FormControlLabel } from '@mui/material';
+ import { FormLabel } from '@mui/material';
+ //import {Checkbox} from '@material-ui/core/Checkbox';
+ import { Checkbox } from '@mui/material';
+ class PostForm extends Component {
 	constructor(props) {
 		super(props)
 
 		this.state = {
-			Event_name: "",
+            Event_name: "",
 			Date: '',
 			
 			
@@ -17,12 +28,14 @@ class PostForm extends Component {
 			
 		}
 	}
+   
+   
 
-	changeHandler = e => {
+
+    changeHandler = e => {
 		this.setState({ [e.target.name]: e.target.value })
 	}
-
-	submitHandler = e => {
+    submitHandler = e => {
 		e.preventDefault()
 		console.log(this.state)
 		
@@ -40,86 +53,63 @@ class PostForm extends Component {
 			.catch(error => {
 				console.log(error)
 			})
-			this.setState({
-				Event_name: "",
-				Date: '',
-				
-				
-				Location:'',
-				Time:'',
-				Organizer: '',
-				
+            this.setState({
+                Event_name: "",
+                Date: '',
+                
+                
+                Location:'',
+                Time:'',
+                Organizer: '',
+                
 			
             });
 	}
-
-	render() {
-		const { Event_name,Date,Location,Time, Organizer} = this.state;
-		return (
-			<div>
-				<form onSubmit={this.submitHandler} style={{textAlign:"center"}}>
-				<div>
-						<label><b>Enter Events you want to show</b> </label>
-						
-					</div>
-						<div  style={{marginRight:"90px"}}>
-						<label>Enter Event_name</label>
-						<input
+    render() {
+   const paperStyle = { padding: '30px 20px', width: 300, margin: "20px auto" }
+    const headerStyle = { margin: 0 }
+    const avatarStyle = { backgroundColor: '#1bbd7e' }
+    const { Event_name,Date,Location,Time, Organizer} = this.state;
+    return (
+        <Grid>
+            <Paper elevation={20} style={paperStyle}>
+                <Grid align='center'>
+                    
+                    <Typography variant='caption' gutterBottom>Please Enter Events you want to show !</Typography>
+                </Grid>
+                <form onSubmit={this.submitHandler}>
+                    <TextField fullWidth label='Event Name' placeholder="Enter your Event name" 
 							type="text"
 							name="Event_name"
 							value={Event_name}
-							onChange={this.changeHandler}
-							style={{marginLeft:"20px"}}
-						/>
-					</div>
-					<div>
-					<label>Enter Date </label>
-						<input
-							type="text"
+							onChange={this.changeHandler} />
+                    <TextField fullWidth type="date"
 							name="Date"
 							value={Date}
-							onChange={this.changeHandler}
-							style={{marginLeft:"20px"}}
-						/>
-					</div>
-					
-				
-					<div>
-					<label>Enter Location </label>
-						<input
-							type="text"
+							onChange={this.changeHandler} />
+                    <TextField fullWidth label='Location' placeholder="Enter your Location" type="text"
 							name="Location"
 							value={Location}
-							onChange={this.changeHandler}
-							style={{marginLeft:"20px"}}
-						/>
-					</div>
-					<div>
-					<label>Enter Time </label>
-						<input
-							type="text"
+							onChange={this.changeHandler} />
+                 
+                   
+                    
+                    <TextField fullWidth label='Time' placeholder="Enter your time for the Event" type="time"
 							name="Time"
 							value={	Time}
-							onChange={this.changeHandler}
-							style={{marginLeft:"20px"}}
-						/>
-					</div>
-					<div>
-					<label>Enter Organizer </label>
-						<input
-							type="text"
+							onChange={this.changeHandler} />
+                    <TextField fullWidth label='Organizer' placeholder="Enter your Organizer" type="text"
 							name="Organizer"
 							value={	Organizer}
-							onChange={this.changeHandler}
-							style={{marginLeft:"20px"}}
-						/>
-					</div>
-					
-					<button type="submit">Submit</button>
-				</form>
-			</div>
-		)
-	}
-}
+							onChange={this.changeHandler}/>
+              
+                   
+                   
+                    <Button type='submit' variant='contained' color='primary' style={{marginTop: "15px",marginLeft:"90px"}}>Submit Event</Button>
+                </form>
+            </Paper>
+        </Grid>
+    )
+}}
 
-export default PostForm
+export default PostForm;
