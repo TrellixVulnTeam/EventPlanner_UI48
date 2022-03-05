4523ddc1b25ae8f770c6c10485d828e083cbd2cc
DikshaVerma25 <71563921+DikshaVerma25@users.noreply.github.com>
2022-03-04 21:16:55 -0500

    Updated test cases

--- Client/WebPage/cypress/integration/homepage.js
+++ Client/WebPage/cypress/integration/homepage.js
@@ -1,6 +1,19 @@
 // import { cyan } from "@material-ui/core/colors"
 
 describe("renders the home page", () => {
+    it("click login ",() =>
+    {
+        cy.visit("/")
+        cy.get('#login').should('contain.text', 'Login');
+        
+    });
+    it("click signup",() =>
+    {
+        cy.visit("/")
+        cy.get('#signup').should('contain.text', 'SignUp');    
+        cy.get('button').should('contain','Button')
+         });
+
     it("renders correctly", () => {
         cy.visit("/")
         cy.get('#login').click();
@@ -9,18 +22,6 @@ describe("renders the home page", () => {
         cy.get('#mui-2').type("12345678");
         cy.get('.MuiButton-root').click();
 
-
     });
-    // it("click login ",() =>
-    // {
-    //     cy.visit("/")
-    //     cy.get('#login').should('contain.text', 'Login');
-        
-    // });
-    // it("click signup",() =>
-    // {
-    //     cy.visit("/")
-    //     cy.get('#signup').should('contain.text', 'SignUp');    
-    // });
 
 });
