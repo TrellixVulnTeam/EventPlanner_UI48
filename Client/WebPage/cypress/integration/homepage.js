f4ede51ea24b3efacdcc0c5a0298b8283927dc06
DikshaVerma25 <71563921+DikshaVerma25@users.noreply.github.com>
2022-03-04 19:33:31 -0500

    Test cases end to end

--- Client/WebPage/cypress/integration/homepage.js
+++ Client/WebPage/cypress/integration/homepage.js
@@ -1,15 +1,26 @@
-import { cyan } from "@material-ui/core/colors"
+// import { cyan } from "@material-ui/core/colors"
 
 describe("renders the home page", () => {
     it("renders correctly", () => {
         cy.visit("/")
-        cy.get("#container").should("exist")
-    });
-    it("allows the date picker to be used",() =>
-    {
-        cy.visit("/")
-    })
-    
-});
+        cy.get('#login').click();
+        cy.wait(2000);
+        cy.get('#mui-1').type("dikshaverma235@gmail.com");
+        cy.get('#mui-2').type("12345678");
+        cy.get('.MuiButton-root').click();
 
-    
+
+    });
+    // it("click login ",() =>
+    // {
+    //     cy.visit("/")
+    //     cy.get('#login').should('contain.text', 'Login');
+        
+    // });
+    // it("click signup",() =>
+    // {
+    //     cy.visit("/")
+    //     cy.get('#signup').should('contain.text', 'SignUp');    
+    // });
+
+});
