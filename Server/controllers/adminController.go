1b105c7b358fbfe4d64269f1be4280dbece956dd
rahul bhatia <rahul.1517.bhatia@gmail.com>
2022-03-28 15:09:03 -0400

    Added crud using postgres

--- Server/controllers/adminController.go
+++ Server/controllers/adminController.go
@@ -1,15 +1,48 @@
 package controllers
 
 import (
+	"database/sql"
 	"fmt"
-
+	_ "github.com/lib/pq"
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "pass"
+	dbname   = "tripplanner"
+)
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Connect() {
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+
+	defer db.Close()
+
+	// insert
+	// hardcoded
+	insertStmt := `insert into "students"("id","name", "roll") values(1,'John', 1)`
+	_, e := db.Exec(insertStmt)
+	CheckError(e)
+	
+}
+
 func PushNotifs() gin.HandlerFunc {
+
 	return func(c *gin.Context) {
 
 		fmt.Println("Log Active users")
 		fmt.Println("wait for trigger")
+		Connect()
 	}
 }
