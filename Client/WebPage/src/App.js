af4913879c2b0a12d0ad2254ea055bb4af5bcd79
Jenil jain <54360821+jenil13@users.noreply.github.com>
2022-03-31 14:47:02 -0400

    Update App.js

--- Client/WebPage/src/App.js
+++ Client/WebPage/src/App.js
@@ -1,25 +1,79 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 //import Footer from ".components/Footer";
+import react ,{Component} from 'react'
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Events from "./pages/Events";
 import Signup from "./pages/SignUp";
+import  DisplayEvent from "./pages/DisplayEvent";
+import  Forgot from "./pages/Forgotcomponent";
+
 import {BrowserRouter as Router, Routes, Route} 
 from 'react-router-dom';
+import axios from 'axios'
+
+export default class App extends Component
+ {
+    state = {
+       user: []
+      }
+  componentDidMount(){
 
 
-function App() {
+    var url = 'http://localhost:9000/users?user_id=' + localStorage.getItem('token1');
+    // var url = 'http://localhost:9000/event';
+    
+    
+     axios.get(url, {
+        headers :{
+          'Content-Type' : 'application/x-www-form-urlencoded',
+          Accept: 'application/json',
+           'Content-Type': 'application/json',
+          
+
+
+
+          token : localStorage.getItem('token'),
+
+        }}).then(
+
+           res =>{
+            //  console.log(res.data);
+            // this.setState({
+            //   user: res.data
+            // });
+            //     console.log(res);
+
+            this.setUser(res.data);
+           },
+           err => {
+               console.log(err)
+           }
+    )
+};
+  setUser = user=>{
+    this.setState({
+      user: user
+    });
+  }
+   render(){
+     console.log("i am jenil.")
+     console.log(this.state.user);
   return (
     <div className="App">
 
       <Router>
-        <Navbar> </Navbar>
+        <Navbar user={this.state.user} setUser={this.setUser}> </Navbar>
+        
         <Routes>
-          <Route path ="/" element={<Home/>}/>
+        {/* <Route path ="/" component={()=><Home user={this.state.user}/>}/> */}
+          <Route path ="/" element={<Home user={this.state.user}/>}/>
           <Route path ='/event' element={<Events/>}/>
-          <Route path ='/Login'  element={<Login/>}/>
+          <Route path ='/Login'  element={<Login setUser={this.setUser}/>}/>
           <Route path ='/Signup'  element={<Signup/>}/>
+          <Route path ='/about'  element={<DisplayEvent/>}/>
+          {/* <Route path ='/forgot'  element={<Forgot/>}/> */}
       </Routes>
       </Router>  
     
@@ -27,6 +81,5 @@ function App() {
     </div>
       
   );
+   }
 }
-
-export default App;
