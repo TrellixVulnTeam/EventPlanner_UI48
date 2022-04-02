29146c83fe288a10fbb33c54c5b016a91a338be2
Jenil jain <54360821+jenil13@users.noreply.github.com>
2022-04-01 20:03:45 -0400

    Notification message

---
+++ Client/WebPage/src/pages/Notifs.js
@@ -0,0 +1,136 @@
+import axios from "axios";
+import React , {Component}from "react";
+
+import { Grid, Paper, Avatar, Typography, TextField, Button } from '@mui/material'
+import { Label } from "reactstrap";
+
+
+
+export default class Notifs extends Component  {
+
+    state={
+        type: "sale",
+        message: ""
+    }
+    changeHandler = e => {
+		this.setState({ [e.target.name]: e.target.value })
+	}
+    submitHandler = e => {
+		e.preventDefault()
+		console.log(this.state)
+		
+		
+     
+		// axios
+		// 	.post('http://localhost:9000/admin/notifs', this.state.message,{
+		// 		headers :{
+		// 			'Content-Type' : 'application/x-www-form-urlencoded',
+		// 			Accept: 'application/json',
+		// 			 'Content-Type': 'application/json',
+		// 			token : localStorage.getItem('token'),
+		// 	}}).then(response => {
+        //          console.log("yippee")
+        //         console.log(response)
+		// 	})
+		// 	.catch(error => {
+		// 		console.log(error)
+		// 	})
+            }
+    render(){
+        const { message } = this.state;
+        const paperStyle = { padding: '30px 20px', width: 300, margin: "20px auto" }
+        let buttons1;
+        if(this.state.type == "sale"){
+
+            
+
+             buttons1 = (  <Grid>
+                 
+                         <Paper elevation={20} style={paperStyle}>
+                         <Grid align='center'>
+                    
+                           < Typography variant='caption' gutterBottom>Please messages for notification !</Typography>
+                         </Grid>
+                         <form onSubmit={this.submitHandler}>
+                      <TextField style={{color:"white"}} fullWidth label='Enter messsage for notification' placeholder="Enter messsage for notification" type="text"
+							name="message"
+							value={message}
+							onChange={this.changeHandler} />
+            
+                      <Button type='submit' variant='contained' color='primary' style={{marginTop: "15px",marginLeft:"90px"}}>Post noitification</Button>
+                            </form>
+                            </Paper>
+                            </Grid>
+                         )
+                    }
+            else{
+                      buttons1=(   <h1 style={{color:"white"}}>{this.state.type}</h1>)
+                 }
+    
+
+    return(
+
+                    <div >
+                        <Label style={{color:"white"}}>Select type of event :   </Label>
+                        <select value={this.state.type} onChange={(e) =>{
+                             
+                             this.setState({ type:  e.target.value });
+                             
+                        }}> 
+                        
+                        <option value = "sale">SALE</option>
+                        <option value ="event_attended"> event_attended</option>
+                        
+                        </select>
+                       
+                        {buttons1}
+                    </div>
+                )
+
+}}
+// function Notifs(){
+       
+
+
+//        const [type,setType] = useState("sale");
+    
+//         let buttons1 ;
+
+//         if(type == "sale"){
+//              buttons1 = (
+
+//                 // <TextField fullWidth label='text for notifications' placeholder="Enter your text" type='password' 
+//                 //              name="Password"
+//                 //             value={	Password}
+//                 //         onChange={this.changeHandler}/>
+
+//                 <h1 style={{color:"white"}}>{type}</h1>
+
+//              )
+//         }
+//         else{
+//             <h1 style={{color:"white"}}>{type}</h1>
+//         }
+
+//         return(
+
+//             <div >
+//                 <select value={type} onChange={(e) =>{
+//                      const selected = e.target.value;
+//                      setType(selected);
+                     
+//                 }}> 
+                
+//                 <option value = "sale">SALE</option>
+//                 <option value ="event_attended"> event_attended</option>
+                
+//                 </select>
+
+//                 {buttons1}
+//             </div>
+//         )
+
+
+// }
+
+// export default Notifs;
