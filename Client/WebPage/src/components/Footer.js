b562c329861a2f275a3f26e60818088131817949
DikshaVerma25 <dikshaverma235@gmail.com>
2022-04-01 20:11:18 -0400

    Footer Redesign & Ticket Fucntions Updated

--- Client/WebPage/src/components/Footer.js
+++ Client/WebPage/src/components/Footer.js
@@ -1,55 +1,119 @@
-import styled from 'styled-components';
+import React from 'react';
+import '../styles/Footer.css';
+import { Link } from 'react-router-dom';
 
-export const Container = styled.div`
-  padding: 80px 60px;
-  background: radial-gradient(circle, rgba(92,39,251,1) 0%, rgba(112,71,247,1) 100%);
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <section className='footer-subscription'>
+        <p className='footer-subscription-heading'>
+          Join the Adventure newsletter to receive our best events deals
+        </p>
+        <p className='footer-subscription-text'>
+          You can unsubscribe at any time.
+        </p>
+        <div className='input-areas'>
+          <form>
+            <input
+              className='footer-input'
+              name='email'
+              type='email'
+              placeholder='Your Email'
+            />
+            {/* <Button buttonStyle='btn--outline'>Subscribe</Button> */}
+          </form>
+        </div>
+      </section>
+      <div class='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>About Us</h2>
+            <Link to='/sign-up'>How it works</Link>
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Contact Us</h2>
+            <Link to='/'>Contact</Link>
+            <Link to='/'>Support</Link>
+            <Link to='/'>Destinations</Link>
+            <Link to='/'>Sponsorships</Link>
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>Videos</h2>
+            <Link to='/'>Submit Video</Link>
+            <Link to='/'>Ambassadors</Link>
+            <Link to='/'>Agency</Link>
+            <Link to='/'>Influencer</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Social Media</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>Facebook</Link>
+            <Link to='/'>Youtube</Link>
+            <Link to='/'>Twitter</Link>
+          </div>
+        </div>
+      </div>
+      <section class='social-media'>
+        <div class='social-media-wrap'>
+          <div class='footer-logo'>
+            <Link to='/' className='social-logo'>
+              TRVL
+              <i class='fab fa-typo3' />
+            </Link>
+          </div>
+          <small class='website-rights'>TRVL © 2020</small>
+          <div class='social-icons'>
+            <Link
+              class='social-icon-link facebook'
+              to='/'
+              target='_blank'
+              aria-label='Facebook'
+            >
+              <i class='fab fa-facebook-f' />
+            </Link>
+            <Link
+              class='social-icon-link instagram'
+              to='/'
+              target='_blank'
+              aria-label='Instagram'
+            >
+              <i class='fab fa-instagram' />
+            </Link>
+            <Link
+              class='social-icon-link youtube'
+              to='/'
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <i class='fab fa-youtube' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='Twitter'
+            >
+              <i class='fab fa-twitter' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='LinkedIn'
+            >
+              <i class='fab fa-linkedin' />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
 
-  @media (max-width: 1000px) {
-    padding: 70px 30px;
-  }
-`;
-
-export const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    max-width: 1000px;
-    margin: 0 auto;
-    /* background: red; */
-`
-
-export const Column = styled.div`
-  display: flex;
-  flex-direction: column;
-  text-align: left;
-  margin-left: 60px;
-`;
-
-export const Row = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
-  grid-gap: 20px;
-
-  @media (max-width: 1000px) {
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  }
-`;
-
-export const Link = styled.a`
-  color: #fff;
-  margin-bottom: 20px;
-  font-size: 18px;
-  text-decoration: none;
-
-  &:hover {
-      color: #ff9c00;
-      transition: 200ms ease-in;
-  }
-`;
-
-export const Title = styled.p`
-  font-size: 24px;
-  color: #fff;
-  margin-bottom: 40px;
-  font-weight: bold;
-`;
+export default Footer; 
--- Client/WebPage/src/pages/Ticket.js
+++ Client/WebPage/src/pages/Ticket.js
@@ -1,1 +1,85 @@
+import axios from "axios";
+import React, { Component } from 'react'
+import {Link} from "react-router-dom";
+import BannerImage from "../assets/image.jpeg";
 
+export default class Ticket extends Component {
+    componentDidMount(){
+        var url1 = 'http://localhost:9000/event';
+        
+         axios.get(url1, {
+            headers :{
+              'Content-Type' : 'application/x-www-form-urlencoded',
+              Accept: 'application/json',
+               'Content-Type': 'application/json',
+              token : localStorage.getItem('token'),
+  
+            }}).then(
+
+               res =>{
+
+               this.setState({ persons: res.data[0].user_items });        
+               },
+               err => {
+                   //console.log(err)
+               }
+        )
+    }
+    axios
+			.post('http://localhost:9000/ticket', this.state,{
+				headers :{
+				  'Content-Type' : 'application/x-www-form-urlencoded',
+                  
+                   
+	  
+				}
+			})
+			.then(response => {
+
+
+                // if(response.data.token){
+                //     alert("logged in successfull")
+                // }
+
+                localStorage.setItem('token', response.data.token);
+                 localStorage.setItem('token1', response.data.user_id);
+				this.setState({
+					LoggedIn: true
+				});
+
+			  this.props.setUser(response.data);
+				console.log("RESPONES")
+                console.log(response.data);
+
+				// console.log(response.data.token)
+			}
+
+	}
+    render(){
+        console.log("hele")
+        console.log(this.state.persons )
+        if(this.state.persons.length !=0 ){
+            return(  <div className="container"  >  {   this.state.persons
+                .map(person =>
+                   <div className="event" key={person._id}>
+                        <div>
+                        <p>Organizer : {person.organizer}</p>
+                        <p>Time : {person.time}</p>
+                        </div>
+                        <div style={{color:"white",paddingLeft:"5px"}}>
+                        {person.date}
+                        </div>
+
+                        <div>
+                         <span>{person.event_name}</span>
+                        <h3>{person.location}</h3>
+                        </div>
+                         
+                       </div>
+                )
+       
+                } </div>)
+
+}
+}
+}
