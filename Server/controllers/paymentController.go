d5c8036ab9f513864a417b9b9dbcd5b3e2304584
simran bhagwandasani <simirb98@gmail.com>
2022-04-15 12:06:46 +0530

    added Stripe price id

--- Server/controllers/paymentController.go
+++ Server/controllers/paymentController.go
@@ -6,18 +6,28 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/stripe/stripe-go/v72"
+  "github.com/stripe/stripe-go/v72/price"
 	"github.com/stripe/stripe-go/v72/checkout/session"
 )
 
 func CreateCheckoutSession() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		stripe.Key = "sk_test_51Ko7SIBldx1WlAU0CepzRr8Ka4TN9tZ87K3nfaUxvX8wbJVW3TwBj7nBKJPv20rPrlAUyvjkW95J474XYBYQKNRa00kjYL8SWB"
-		domain := "http://localhost:4242"
+		domain := "http://localhost:9000"
+    priceParams := &stripe.PriceParams{
+      Currency : stripe.String(string(stripe.CurrencyUSD)),
+      unitAmount: stripe.Int64(10),
+      product: stripe.String("prod_LV8PWWYl9PM1FF")
+    }
+    p, _ := price.New(priceParams)
+
 		params := &stripe.CheckoutSessionParams{
 			LineItems: []*stripe.CheckoutSessionLineItemParams{
 				&stripe.CheckoutSessionLineItemParams{
 					// Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-					Price:    stripe.String("price_1Ko891Bldx1WlAU0bRYnUCpY"),
+          PriceData: 
+					Amount:    stripe.Int64(10),
+          Currency: stripe.String(string(stripe.CurrencyUSD))
 					Quantity: stripe.Int64(1),
 				},
 			},
