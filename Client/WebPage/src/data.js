2706c004f726f019e605d4296172a3c6c8bdae21
DikshaVerma25 <71563921+DikshaVerma25@users.noreply.github.com>
2022-04-23 23:57:17 -0400

    Create data.js

---
+++ Client/WebPage/src/data.js
@@ -0,0 +1,38 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'Gator Night',
+            date: '2016-01-23',
+            price: 5,
+            location: 'Reitz Union',
+            image: "https://image.shutterstock.com/image-vector/white-events-sign-over-confetti-600w-300283754.jpg"
+        },
+        {
+            id: '2',
+            name: 'Gator Dining',
+            date: '2016-01-28',
+            price: 10,
+            location: 'Reitz Union',
+            image: "https://image.shutterstock.com/image-vector/white-events-sign-over-confetti-600w-300283754.jpg"
+        },
+        {
+            id: '3',
+            name: 'Soccer Match',
+            date:'2016-01-25',
+            price: 20,
+            location: 'Flavet Ground',
+            image: "https://image.shutterstock.com/image-vector/white-events-sign-over-confetti-600w-300283754.jpg"
+        },
+        {
+            id: '4',
+            name: 'Holi',
+            date:'2016-01-25',
+            price: 15,
+            location: 'Flavet Field',
+            image: "https://image.shutterstock.com/image-vector/white-events-sign-over-confetti-600w-300283754.jpg"
+        },
+    ],
+};
+
+export default data;
